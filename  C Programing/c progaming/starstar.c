4596ca98110a5e534fdbed176875754327258d2b
Rohan-1939 <headhunter101010@gmail.com>
2025-03-26 11:20:56 +0530

    3

---
+++  C Programing/c progaming/starstar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {   
+        printf("%d\n%d\n*\n*\n",i,i+1);
+        i++;
+        /*printf("*\n*\n%d \n%d \n",a,b);
+        a=a+2;
+        b=b+2;*/
+        
+    }
+}
