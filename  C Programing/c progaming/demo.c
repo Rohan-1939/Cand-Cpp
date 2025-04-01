d0b7c6591d4e4ee15b3234a2a7e5a80e22210d3c
Rohan-1939 <headhunter101010@gmail.com>
2025-04-01 11:32:35 +0530

    10

---  C Programing/c progaming/demo.c
+++  C Programing/c progaming/demo.c
@@ -3,18 +3,18 @@
 void main()
 {
     int i, j, a[10], element, found=0;
-    printf("Enter any number in array only 5");
+    printf("Enter any number in array only 5 \n");
     for (i = 0; i < 5; i++)
     {
         scanf("%d", &a[i]);
     }
-    printf("Enter any number you want to search");
+    printf("\n Enter any number you want to search ");
     scanf("%d", &element);
     for (i = 0; i < 5; i++)
-    {
+    { 
         if (element == a[i])
         {
-            printf("Enter element is present at %d position \n", i);
+            printf("Enter element is present at %d position \n", i+1);
             found=1;
             break;
         }
